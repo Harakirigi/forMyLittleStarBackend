c6629e328721e8c2d2f7c52e59dc9e7eb64ee7ba
Harakirigi <nkomyt2@gmail.com>
2025-03-22 01:07:15 +0500

    LAST

--- api/server.js
+++ api/server.js
@@ -8,12 +8,11 @@ require('dotenv').config();
 // Global variables
 const app = express();
 const PORT = process.env.PORT || 3000;
-const starRoutes = require("./routes/starRoutes");
+const starRoutes = require("../routes/starRoutes");
 
 // App use
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "dist")));
 app.use("/star", starRoutes);
 
 
@@ -21,23 +20,13 @@ app.use("/star", starRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "FRONTEND/build")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "FRONTEND", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "FRONTEND", "build", "index.html"));
     });
-}
-
-
-// Requests
+  }
+  
+// Default route
 app.get("/", (req, res) => {
-  res.send("Hello from Backend!");
+    res.send("Hello from Backend!");
 });
 
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../FRONTEND", "index.html"));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = app;
+module.exports = (req, res) => app(req, res);
